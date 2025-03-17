d42a56bc2a9ae4c4a17b33e6e0330f41203cbf6d
Aman Prasad <apssamrat07@gmail.com>
2025-03-17 17:13:20 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Toaster, toast } from 'react-hot-toast';
 import { CheckSquare } from 'lucide-react';
 import { TaskForm } from './components/TaskForm';
